914dad413743f12bc0a4beb77cd4f6254b7be9ca
Michelangelo Mastrorocco <mastroroccodegrandis@gmail.com>
2020-10-13 17:09:36 +0200

    agewall

---
+++ dist/js/age_validification.js
@@ -0,0 +1,9 @@
+onload = function() {
+    $('section').css('filter', 'blur(12px)');
+    $('header').css('filter', 'blur(12px)');
+    $('footer').css('filter', 'blur(12px)');
+    $('section').css('-webkit-filter', 'blur(12px)');
+    $('header').css('-webkit-filter', 'blur(12px)');
+    $('footer').css('-webkit-filter', 'blur(12px)');
+    
+}
